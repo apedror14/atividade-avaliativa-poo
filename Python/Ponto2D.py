bb769cca082e8113d4c1bc4a523230b47b29fea4
Murilo Sousa <murilosf789@gmail.com>
2026-05-18 18:39:48 -0300

    Refine comments in Ponto2D class
    Updated comments in Ponto2D class for clarity.

--- Python/Ponto2D.py
+++ Python/Ponto2D.py
@@ -3,39 +3,39 @@ import math
 
 class Ponto2D:
 
-    # O construtor recebe as coordenadas x e y do ponto
+    # O construtor coordenada
     def __init__(self, abcissa=0.0, ordenada=0.0):
         self.__abcissa = abcissa
         self.__ordenada = ordenada
 
-    # Retorna o valor de x
+    # Retorna x
     def getAbcissa(self):
         return self.__abcissa
 
-    # Atualiza o valor de x
+    # Atualiza x
     def setAbcissa(self, abcissa):
         self.__abcissa = abcissa
 
-    # Retorna o valor de y
+    # Retorna y
     def getOrdenada(self):
         return self.__ordenada
 
-    # Atualiza o valor de y
+    # Atualiza y
     def setOrdenada(self, ordenada):
         self.__ordenada = ordenada
 
-    # Move o ponto para uma nova posição 
+    # Move Ponto
     def mover(self, abcissa, ordenada):
         self.__abcissa = abcissa
         self.__ordenada = ordenada
 
-    # Calcula a distância
+    # Calcula distância
     def calcularDistancia(self, outroPonto):
         deltaX = self.__abcissa - outroPonto.getAbcissa()
         deltaY = self.__ordenada - outroPonto.getOrdenada()
         return math.sqrt(deltaX ** 2 + deltaY ** 2)
 
-    # Cria um novo ponto 
+    # Cria ponto 
     def clonar(self):
         return Ponto2D(self.__abcissa, self.__ordenada)
 
