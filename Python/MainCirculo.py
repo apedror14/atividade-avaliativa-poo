f857b5acbb22884dea8a8c054e82a8fab1fc795e
Eduardo Santrovitsch <esantrovitsch@gmail.com>
2026-05-18 15:53:11 -0300

    Add files via upload

---
+++ Python/MainCirculo.py
@@ -0,0 +1,19 @@
+import Circulo
+
+c1 = Circulo.Circulo(5, 10, 10)
+c2 = Circulo.Circulo(8, 2, 5)
+
+c1.imprimirDados("Círculo 1")
+c2.imprimirDados("Círculo 2")
+
+c1.inflar()
+c2.inflar()
+
+c1.imprimirDados("Círculo 1")
+c2.imprimirDados("Círculo 2")
+
+c1.desinflar()
+c2.desinflar()
+
+c1.imprimirDados("Círculo 1")
+c2.imprimirDados("Círculo 2")
