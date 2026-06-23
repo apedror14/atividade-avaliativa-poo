537335336053f3b95eff81c69e331818fe6a58c1
pdro <antoniopedro.russoapedro@gmail.com>
2026-06-22 21:24:49 -0300

    Create Figura.java

---
+++ avancado-intermediario/Java/Q3/Figura.java
@@ -0,0 +1,26 @@
+public class Figura{
+  private double x;
+  private double y;
+
+   public Figura(double x, double y) {
+        System.out.println("Criando figura");
+   }
+  
+public double getX(){
+  return x;
+}
+  
+public void setX(){
+  this.x = x;
+}
+  public double getY(){
+  return y;
+}
+  
+public void setY(){
+  this.y = y;
+}
+  public string desenhar(){
+    System.out.println("Desenhando uma figura na posição (" + x + ", " + y + ")");
+    }
+  
