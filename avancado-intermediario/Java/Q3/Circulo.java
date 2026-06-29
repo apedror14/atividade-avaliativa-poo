349fc97765b91d5f760f48c341008ad690278474
pdro <antoniopedro.russoapedro@gmail.com>
2026-06-29 13:08:42 -0300

    Update Circulo.java

--- avancado-intermediario/Java/Q3/Circulo.java
+++ avancado-intermediario/Java/Q3/Circulo.java
@@ -3,13 +3,11 @@ public class Circulo extends Figura {
 
     public Circulo(double x, double y, double raio) {
         super(x, y);
-        System.out.println("Criando círculo");
         this.raio = raio;
+        System.out.println("Criando círculo");
     }
 
     public void desenhar() {
-        System.out.println(
-            "Desenhando um círculo na posição (" + getX() + ", " + getY() + ") com raio de " + raio
-        );
+        System.out.println("Desenhando um círculo na posição (" + getX() + ", " + getY() + ") com raio de " + raio);
     }
 }
