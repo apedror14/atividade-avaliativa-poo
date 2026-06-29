54e06b14af001f34ca5cbe89c13653a2c44d1868
pdro <antoniopedro.russoapedro@gmail.com>
2026-06-29 13:30:33 -0300

    Update Figura.java

--- avancado-intermediario/Java/Q3/Figura.java
+++ avancado-intermediario/Java/Q3/Figura.java
@@ -1,29 +1,32 @@
-public class Figura{
-  private double x;
-  private double y;
+public class Figura {
+    private double x;
+    private double y;
 
-public Figura(double x, double y) {
-  System.out.println("Criando figura");
-  this.x = x;
-  this.y = y;
-   }
-  
-public double getX(){
-  return this.x;
+    public Figura(double x, double y) {
+        this.x = x;
+        this.y = y;
+        System.out.println("Criando figura...");
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void desenhar() {
+        System.out.println(
+            "Desenhando uma figura na posição (" + x + ", " + y + ")"
+        );
+    }
 }
-  
-public void setX(){
-  this.x = x;
-}
-  public double getY(){
-  return this.y;
-}
-  
-public void setY(){
-  this.y = y;
-}
-  public String desenhar(){
-    System.out.println("Desenhando uma figura na posição (" + x + ", " + y + ")");
-  }
-}
-  
